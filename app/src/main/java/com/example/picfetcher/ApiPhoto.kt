8b1010cfa35374dcf21d639cca78d11e680dc62d
Alireza Faraji <farajialireza82@gmail.com>
2023-12-04 22:53:07 +0330

    replaced Moshi with Gson

--- app/src/main/java/com/example/picfetcher/ApiPhoto.kt
+++ app/src/main/java/com/example/picfetcher/ApiPhoto.kt
@@ -1,14 +1,19 @@
 package com.example.picfetcher
 
+import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.Json
 
 data class ApiPhoto(
     @field:Json(name = "albumId")
+    @SerializedName("albumId")
     val albumId: Int,
     @field:Json(name = "id")
+    @SerializedName("id")
     val id: Int,
     @field:Json(name = "url")
+    @SerializedName("url")
     val url: String,
     @field:Json(name = "thumbnailUrl")
+    @SerializedName("thumbnailUrl")
     val thumbnailUrl: String
 )
--- app/src/main/java/com/example/picfetcher/RetrofitClient.kt
+++ app/src/main/java/com/example/picfetcher/RetrofitClient.kt
@@ -1,9 +1,8 @@
 package com.example.picfetcher
 
-import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
@@ -12,7 +11,7 @@ object RetrofitClient {
     private fun getClientInstance() =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
 
